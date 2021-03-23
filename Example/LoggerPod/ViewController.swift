740bd7b66e934bc7519cefd8a9da42ce77aed273
Rohan Dilip Sanap <sanaprohan10@gmail.com>
2021-03-23 19:36:32 +0530

    Add dummy class to LoggerPod

--- Example/LoggerPod/ViewController.swift
+++ Example/LoggerPod/ViewController.swift
@@ -7,18 +7,16 @@
 //
 
 import UIKit
+import LoggerPod
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    let someObject = LoggerPodDummy()
+    print(someObject.name)
+  }
 
 }
 
---
+++ LoggerPod/Classes/LoggerPodDummy.swift
@@ -0,0 +1,14 @@
+//
+//  LoggerPodDummy.swift
+//  LoggerPod
+//
+//  Created by Rohan on 23/03/21.
+//
+
+import Foundation
+
+public class LoggerPodDummy {
+  public let name = "LoggerPodDummy"
+
+  public init() {}
+}
